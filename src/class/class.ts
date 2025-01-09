649827faa6d4a587b322475092c79fd7ea4b63f6
sungwoonko <ko_sw@naver.com>
2025-01-09 15:52:16 +0900

    클래스 사용 양식 최종 정리

--- src/class/class.ts
+++ src/class/class.ts
@@ -4,7 +4,7 @@ class Robot1 { //Members
     // Property 또는 Field(속성, 필드)
     name: string;
     model: string;
-    status: string = "Active";
+    status: string = "Active"; 
 
     // Constructor(생성자)
     constructor(name: string, model: string){
@@ -54,3 +54,36 @@ class Pet{
 
 let p1 = new Pet();
 console.log(p1.name);
+
+
+class User {
+    // 필드 부분
+    username: string; // 타입만을 표기하는 경우
+    email: string; 
+    job:string = "student"; // 기본 값을 주는 경우
+
+    // 생성자 부분
+    constructor(user: string, email: string){
+        this.username = this.username;
+        this.email = email;
+    }
+
+    // 메서드 부분
+    study(): void {
+        console.log(`${this.username} is studying`);
+    }
+
+}
+
+// 객체 생성 (new 키워드와 arguments 입력)
+let user1 = new User("홍길돌","djfkdfj@naver.com");
+let user2 = new User("홍길디","erere@naver.com");
+
+// 객체의 사용, 접근(.도트연산자 사용)
+// 1. 인스턴스 자체 접근
+console.log(user1); // 출력 : User{username: '홍길동', email: '...'}
+// 2. 인스턴스의 세부 필드 접근
+console.log(user2.username); // 출력 : 김철수
+// 3. 메서드 호출
+user1.study();
+user2.study();
